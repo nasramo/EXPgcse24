46a1b0b85debe8a4422a13c29e729df39601a753
nasramo <103969243+nasramo@users.noreply.github.com>
2024-03-28 14:45:55 +0000

    attempting to do modal

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ $(document).ready(function(){
 $('.popover-dismiss').popover({
   trigger: 'focus'
 });
+$(document).ready(function(){
+    $("#myModal").modal('show');
+});
+jQuery(document).ready(function($) {
+  if ($.cookie("cacher-modal")) {
+    $("#myModal").modal("dispose");
+  } else {
+    $("#myModal").modal("show");
+  }
+
+  $("#modalno").click(function() {
+    if ($(this).is(":checked")) {
+        $("#myModal").modal("dispose");
+        $.cookie("cacher-modal", true);
+    } else {
+      $.cookie("cacher-modal", false);
+    }
+  })
+});
